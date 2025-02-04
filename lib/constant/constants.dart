50ee4a0fe112b31c2cdd37ed67816530d9b8f480
Mohamed habib Msahel <mohamedhabibmsahel@Mohameds-MacBook-Air-2.local>
2025-02-04 10:48:37 +0100

    first commit

---
+++ lib/constant/constants.dart
@@ -0,0 +1,65 @@
+// constants.dart
+
+class AppConstants {
+  //Login Screen
+  static const String appTitle = "Se connecter.";
+  static const String noAccount = "Pas de compte?";
+  static const String appSubtitle = "Accéder à votre compte";
+  static const String imagePath = 'assets/images/pizza.png';
+  static const String emailHintText = 'exemple@exemple.fr';
+  static const String passwordHintText = '************';
+  static const String forgotPasswordText = 'Mot de passe oublié?';
+  static const String privacyPolicy =
+      'Polituqe de confidentialité (privacy policy)';
+  static const String loremIpsumText =
+      'Ce site est protégé par reCAPTCHA. la Politique de confidentialité et les conditions d\'utilisation de Google s\'appliquent';
+  static const String loginButtonText = 'Se connecter';
+  static const String registerButtonText = 'S\'inscrire';
+  //Inscription Screen
+  static const String creerCompteTitle = "Créez votre compte";
+  static const String creerCompteSubtitle =
+      "En vous inscrivant, vous acceptez les";
+  static const String creerCompteSubtitle2 = 'termes d\'utilisation';
+  static const String creerComptePrenom = 'Votre prénom';
+  static const String creerCompteNom = 'Votre nom';
+  static const String creerCompteAdresseMail = 'Votre adresse mail';
+  static const String creerCompteMDP = 'Définissez votre mot de passe';
+  static const String creerCompteCMDP = 'Confirmez votre mot de passe';
+  static const String creerMatriculeFiscal = 'Entrer votre matricule fiscale';
+  static const String creerPasseport = 'Entrer votre numéro de passeport';
+  static const String creerNumFreelancer = 'Entrer votre numéro de freelancer';
+  //Inscription2 Screen
+  static const String completeAccountTitle = 'Complétez votre profil';
+  static const String completeAccountSubtitle =
+      'Nous avons besoin de plus d\'informations';
+  static const String adresseHintText = 'Votre adresse';
+  //UserType Screen
+  static const String userTypeIndividual = 'Particulier';
+  static const String userTypeCompany = 'Société';
+  static const String userTypeNormalUser = 'Client Normal';
+  static const String userTypeTitle = 'Choisissez votre type d\'utilisateur';
+  //InscriptionCompanyIndividual Screen
+  static const String identityCard = 'Carte d\'identité';
+  static const String identityPhoto = 'Photo d\'identité';
+  static const String driverLicense = 'Permis B';
+  static const String rib = 'RIB';
+//InscriptionCompanyIndividual2 Screen
+  static const String insurance = 'Assurance';
+  static const String rcPro = 'RCPRO';
+  static const String policyholderCode = 'Adhérant';
+  static const String bioCleaningCertificate = 'Certificat bionettoyage';
+  //Etape1 Screen
+  static const String yourCommande = 'Votre commande';
+  static const String planifie = 'Planifier votre nettoyage';
+  static const String planifier = 'Planifier';
+  static const String hour = 'A quelle heure ?';
+  //Etape4
+
+  static const String ajoutezVosRenseignements = 'Ajouter vos renseignements';
+  static const String typeDeLogement = 'Type de logement';
+  static const String Superficie = 'Superficie';
+  static const String NombreDePiece = 'Nombre de piéce';
+  //Etape5
+
+  static const String VosBesoins = 'Sélectionnez vos besoins';
+}
---
+++ lib/widgets/custom_image_card.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:test_app/constant/app_fonts.dart';
+import 'package:test_app/constant/colors.dart';
+import 'package:test_app/widgets/custom_image_view.dart';
+
+class CustomImageCard extends StatelessWidget {
+  const CustomImageCard(
+      {super.key,
+      required this.image,
+      required this.title,
+      this.width = 50,
+      this.height = 200});
+
+  final String image;
+  final String title;
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      child: ClipRRect(
+        // Wrap the Stack with ClipRRect
+        borderRadius: BorderRadius.circular(25.0), // Set the border radius
+        child: Stack(
+          children: [
+            CustomImageView(
+              width: width,
+              imagePath: image,
+              fit: BoxFit.cover,
+            ),
+            Opacity(
+              opacity: 0.84,
+              child: Container(height: height, color: kBlackColor),
+            ),
+            Center(
+              child: Text(title,
+                  style: AppFonts.x20Regular.copyWith(color: kWhite)),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
